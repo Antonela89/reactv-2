b2b0b4976da69303d7927f3020daeb7c0188e1f9
Antonela <antonelaborgogno@gmail.com>
2023-10-07 16:46:34 -0300

    primer componente

---
+++ src/componentes/Api.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Api = () => {
+    return (
+        <div>
+            <h1>Renegando con las ramas y los merges</h1>
+        </div>
+    )
+}
+
+export default Api
